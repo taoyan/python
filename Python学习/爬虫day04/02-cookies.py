5a87df02342bb503588c797b2639f34bdb4fb1ab
Allen T Yan <allen.t.yan@cn.pwc.com>
2018-10-25 19:50:28 +0800

    nothing

--- Python学习/爬虫day04/02-cookies.py
+++ Python学习/爬虫day04/02-cookies.py
@@ -8,20 +8,20 @@ cookiejar可以自动保存cookie
 
 
 import urllib.request
-import urllib.parse
+from urllib import parse
 from http import cookiejar
 
 login_url = 'https://www.yaozhi.com/login'
 
 login_form_data = {
-    'username':'15566680322',
-    'pwd':'yantyant',
-    'formhash':'FFA9479C87',
-    'backurl':'https%3A%2F%2Fwww.yaozh.com%2F'
+    "username":"15566680322",
+    "pwd":"yantyant",
+    "formhash":"FFA9479C87",
+    "backurl":"https%3A%2F%2Fwww.yaozh.com%2F"
 }
 
 headers = {
-    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
 }
 
 
@@ -33,11 +33,11 @@ cook_handler = urllib.request.HTTPCookieProcessor(cook_jar)
 opener = urllib.request.build_opener(cook_handler)
 
 #post请求,data需要是bytes
-login_bytes = urllib.parse.urlencode(login_form_data).encode('utf-8')
+login_bytes = parse.urlencode(login_form_data).encode('utf-8')
 login_request = urllib.request.Request(login_url, headers=headers, data=login_bytes)
 
 #如果登录成功cookieJar自动保存cookie
-opener.open(login_url)
+opener.open(login_request)
 
 
 #用带有cookieJar的opener去请求
