7f2d927c5b54de475c4200e6bd70f5831ed3d5e0
yant <1136523628@qq.com>
2019-01-20 23:21:20 +0800

    todos

---
+++ Django/makeplan/todos/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib import auth
+from django.contrib.auth.hashers import make_password, check_password
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+def regist(request):
+    if request.method == 'GET':
+        return render(request, 'todos/regist.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        User.objects.create_user(username=name, password= password)
+        return HttpResponseRedirect('/todos/login')
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'todos/login.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=name, password=password)
+        if user:
+            auth.login(request, user)
+            return HttpResponseRedirect('/todos/index')
+        else:
+            return HttpResponse('用户名或密码错误')
+
+
+def index(request):
+    if request.method == 'GET':
+        return render(request, 'todos/home.html')
+    if request.method == 'POST':
+        # if request.POST.get('id') == 'logout':
+        auth.logout(request)
+        return HttpResponseRedirect('/todos/login')
+
