2c564e0a5d61e450a9c19ad0befcb53767bfbb87
yant <1136523628@qq.com>
2019-02-01 01:21:53 +0800

    upload

--- Django/makeplan/videos/urls.py
+++ Django/makeplan/videos/urls.py
@@ -1,8 +1,11 @@
 
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'videos'
 urlpatterns = [
     url('index/',views.index),
+    url('add/',views.add_video),
+    path('download/<str:file_name>/', views.download),
 ]
--- Django/makeplan/videos/views.py
+++ Django/makeplan/videos/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import Video
 from django.http import HttpResponseRedirect, HttpResponse
+from django.http import StreamingHttpResponse
 
 def index(request):
 
@@ -11,3 +12,34 @@ def index(request):
 
     return render(request, 'videos/index.html',{'videos':videos})
     # return HttpResponse("index")
+
+
+def add_video(request):
+    if request.method == 'GET':
+        return render(request,'videos/add_video.html')
+    else:
+        file = request.FILES['myfile']
+        if not file:
+            return HttpResponse("sorry")
+        else:
+            with open('Media/%s' % file.name, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+            return HttpResponse('success')
+
+
+def download(request, file_name):
+
+    def file_iterator(file, chunk_size = 512):
+        with open(file, encoding='utf-8') as f:
+            while True:
+                c = f.read(chunk_size)
+                if c:
+                    yield c
+                else:
+                    break;
+
+    response = StreamingHttpResponse(file_iterator(file_name))
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format(file_name)
+    return response
