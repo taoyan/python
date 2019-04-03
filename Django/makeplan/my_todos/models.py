5e01ec8d033151ecd4040a464b8f2ac095600ee5
Allen T Yan <allen.t.yan@cn.pwc.com>
2019-04-03 15:43:19 +0800

    nothing

--- Django/makeplan/my_todos/models.py
+++ Django/makeplan/my_todos/models.py
@@ -11,7 +11,6 @@ class Todo(models.Model):
     remind_date = models.DateTimeField(null=True)
     icon_index = models.IntegerField(default=0)
     status = models.IntegerField(default=0)
-    finish_type = models.IntegerField(default=0)
 
     user_id = models.IntegerField(blank=False)
     last_modified = models.DateTimeField(blank=False)
@@ -27,7 +26,6 @@ class Todo(models.Model):
         dict["remindDate"] = None if (self.remind_date == None) else self.remind_date.strftime("%Y-%m-%d %H:%M:%S")
         dict["iconIndex"] = self.icon_index
         dict["status"] = self.status
-        dict["finishType"] = self.finish_type
         dict["userId"] = self.user_id
         dict["lastModified"] = self.last_modified.strftime("%Y-%m-%d %H:%M:%S")
         return dict
